71a4a7c27b31d17b93d899a7f9b2524e9fcafd51
aishat-liasu <aishatliasu17965@gmail.com>
2022-07-16 15:28:02 +0100

    feat: create layout component for all the pages

---
+++ src/components/layout.js
@@ -0,0 +1,94 @@
+import * as React from 'react';
+import { Link, useStaticQuery, graphql } from 'gatsby';
+
+import { header, headerLogo, nav, main, activeLink, navList, menuButton, navMobile } from '../styles/layout.module.css';
+import '../styles/global.css';
+import Nav from './nav.js';
+
+const Layout = ({ children, title = false, description = false, path = false, image = false }) => {
+  const [mobileMenu, setMobileMenu] = React.useState(false);
+  const [menuSymbol, setMenuSymbol] = React.useState(`\u2630`);
+
+  const mobileMenuStyle = {
+    display: 'block',
+  };
+
+  function openMobileMenu(e) {
+    e.preventDefault();
+
+    if (mobileMenu) {
+      setMenuSymbol(`\u2630`);
+    } else {
+      setMenuSymbol(`\uD83D\uDDD9`);
+    }
+    setMobileMenu(!mobileMenu);
+  }
+
+  return (
+    <>
+      <header className={header}>
+        <div className={headerLogo}>
+          <Link to="/">Aishat</Link>
+        </div>
+
+        {/*         <nav className={nav}>
+          <ul className={navList}>
+            <li>
+              <Link to="/about" activeClassName={activeLink}>
+                About
+              </Link>
+            </li>
+            <li>
+              <Link to="/resume" activeClassName={activeLink}>
+                Resume
+              </Link>
+            </li>
+            <li>
+              <Link to="/projects" activeClassName={activeLink} partiallyActive={true}>
+                Projects
+              </Link>
+            </li>
+            <li>
+              <Link to="/blog" activeClassName={activeLink} partiallyActive={true}>
+                Blog
+              </Link>
+            </li>
+          </ul>
+        </nav> */}
+
+        {/*         <nav className={navMobile} style={mobileMenu ? mobileMenuStyle: {}}>
+          <ul className={navList}>
+            <li>
+              <Link to="/about" activeClassName={activeLink}>
+                About
+              </Link>
+            </li>
+            <li>
+              <Link to="/resume" activeClassName={activeLink}>
+                Resume
+              </Link>
+            </li>
+            <li>
+              <Link to="/projects" activeClassName={activeLink} partiallyActive={true}>
+                Projects
+              </Link>
+            </li>
+            <li>
+              <Link to="/blog" activeClassName={activeLink} partiallyActive={true}>
+                Blog
+              </Link>
+            </li>
+          </ul>
+        </nav> */}
+        <Nav styleClassName={nav} />
+        <Nav styleClassName={navMobile} mobileMenu={mobileMenu} />
+        <button className={menuButton} onClick={openMobileMenu}>
+          {menuSymbol}
+        </button>
+      </header>
+      <main className={main}>{children}</main>
+    </>
+  );
+};
+
+export default Layout;
