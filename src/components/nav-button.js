b772aa47face0213659d718cf047c11c75bb9930
aishat-liasu <aishatliasu17965@gmail.com>
2022-07-17 15:51:47 +0100

    feat: create nav component to tranverse posts

---
+++ src/components/nav-button.js
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import { Link, navigate } from 'gatsby';
+
+const NavButton = ({ currentIndex = 0, total = 1, list = [] }) => {
+  //currentIndex is the current index of either a  or a project
+  //total is the number of s or projects
+  //list is the array of objects each containing information for each  or project
+
+  function handleClickNavButton(action, e) {
+    e.preventDefault();
+    let prevOrNextItem = '';
+    if (action === 'next') {
+      prevOrNextItem = list[currentIndex - 1];
+    } else {
+      prevOrNextItem = list[currentIndex + 1];
+    }
+
+    navigate('../' + prevOrNextItem.slug.split('/').pop());
+  }
+
+  return (
+    <nav>
+      <button disabled={currentIndex + 1 >= total ? true : false} onClick={(e) => handleClickNavButton('prev', e)}>
+        &lt;
+      </button>
+      <button disabled={currentIndex - 1 < 0 ? true : false} onClick={(e) => handleClickNavButton('next', e)}>
+        &gt;
+      </button>
+    </nav>
+  );
+};
+
+export default NavButton;
