92ace4886830d39721ebf6747d6ad3c7c6003e60
aishat-liasu <aishatliasu17965@gmail.com>
2022-07-31 22:29:45 +0100

    feat: update home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,9 @@
 import * as React from 'react';
 import { list, listItem, hero, heroImgSection, heroTextSection } from '../styles/index.module.css';
 import Layout from '../components/layout';
-import { Link, navigate } from 'gatsby';
-import aishat_liasu_1 from '../images/aishat_liasu.jpg';
+import { navigate } from 'gatsby';
+import aishat_liasu from '../images/aishat_liasu.jpg';
 
-// styles
-
-// markup
 const IndexPage = () => {
   function showAboutPage(e) {
     e.preventDefault();
@@ -21,14 +18,10 @@ const IndexPage = () => {
             <span> My name is </span> <br /> Aishat Liasu
           </h1>
           <h2>I'm a Full Stack Web Developer && Software Engineer</h2>
-          {/*           <ul className={list}>
-            <li className={listItem}>Software Engineer</li>
-            <li className={listItem}>Full Stack Web Engineer</li>
-          </ul> */}
-          <button onClick={showAboutPage}>About Me</button>
+          <button onClick={showAboutPage}>More about me</button>
         </section>
         <section className={heroImgSection}>
-          <img src={aishat_liasu_1} alt="Aishat somewhere in a Mosque at Ikeja, Lagos" />
+          <img src={aishat_liasu} alt="A selfie of Aishat somewhere in a Mosque at Ikeja, Lagos" />
         </section>
       </div>
     </Layout>
