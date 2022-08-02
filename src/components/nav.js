2743fb96f8106c04b40665d00643e7a66621af0f
aishat-liasu <aishatliasu17965@gmail.com>
2022-08-02 20:08:55 +0100

    feat: add social links to the nav bar

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,6 +1,12 @@
 import * as React from 'react';
 import { Link, useStaticQuery, graphql } from 'gatsby';
-import { activeLink, navList } from '../styles/nav.module.css';
+import { navList, socialNav } from '../styles/nav.module.css';
+
+//import social icons
+import linkedinIcon from '../images/in-icon_colour_white.png';
+import twitterIcon from '../images/tw-icon_colour_white.png';
+import githubIcon from '../images/gh-icon_colour_white.png';
+import emailIcon from '../images/em-icon_colour_white.png';
 
 const Nav = ({ styleClassName, mobileMenu = false }) => {
   const mobileMenuStyle = {
@@ -33,6 +39,29 @@ const Nav = ({ styleClassName, mobileMenu = false }) => {
           <Link to="/blog" activeStyle={activeLinkStyle} partiallyActive={true}>
             Blog
           </Link>
+        </li>
+      </ul>
+
+      <ul className={socialNav}>
+        <li>
+          <a href="https://twitter.com/AishatLiasu" target="_blank" rel="noopener noreferrer">
+            <img src={twitterIcon} alt="Twitter Logo" />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/aishat-liasu" target="_blank" rel="noopener noreferrer">
+            <img src={linkedinIcon} alt="LinkedIn Logo" />
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/aishat-liasu" target="_blank" rel="noopener noreferrer">
+            <img src={githubIcon} alt="GitHub Logo" />
+          </a>
+        </li>
+        <li>
+          <a href="mailto:aishatliasu17965@gmail.com" target="_blank" rel="noopener noreferrer">
+            <img src={emailIcon} alt="Email Logo" />
+          </a>
         </li>
       </ul>
     </nav>
