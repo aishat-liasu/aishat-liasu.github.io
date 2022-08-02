6132f6fc32e4ea550e38df29ce2f6703bc112e60
aishat-liasu <aishatliasu17965@gmail.com>
2022-08-02 20:43:57 +0100

    feat: add pathPrefix to enable deployment to gihub

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,14 +2,19 @@ module.exports = {
   siteMetadata: {
     title: `Aishat Liasu`,
     siteUrl: `https://aishatliasu.com`,
-    description: 'Aishat Liasu | Full Stack Web Developer'
+    description: 'Aishat Liasu | Full Stack Web Developer',
   },
-  plugins: ["gatsby-plugin-react-helmet", "gatsby-plugin-mdx", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "posts",
-      "path": `${__dirname}/src/posts`
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-mdx',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'posts',
+        path: `${__dirname}/src/posts`,
+      },
+      __key: 'posts',
     },
-    __key: "posts"
-  }]
+  ],
+  pathPrefix: '/portfolio',
 };
