ef3a2aaa1bd0ae20c75cd7ca80f17161bbacaa47
aishat-liasu <aishatliasu17965@gmail.com>
2022-08-02 20:07:40 +0100

    feat: add hover feature to the post card

--- src/components/blog-post.js
+++ src/components/blog-post.js
@@ -1,16 +1,22 @@
 import * as React from 'react';
-import { Link, useStaticQuery, graphql } from 'gatsby';
+import { Link, navigate } from 'gatsby';
 import { blogPost } from '../styles/blog-post.module.css';
 
 const BlogPost = ({ title, description, date, slug, id }) => {
+  const postLink = slug.split('/').pop() || '#';
+
+  function showPostPage() {
+    navigate(postLink);
+  }
+
   return (
-    <li className={blogPost}>
-      <img src="" alt="" width="300" height="300" />
+    <li className={blogPost} onClick={showPostPage}>
+      {/* <img src="" alt="" width="300" height="300" /> */}
       <article>
         <h3>{title}</h3>
         <small>{date}</small>
         <p>{description}</p>
-        <Link to={slug.split('/').pop()}>Read More &#8594;</Link>
+        <Link to={postLink}>Read More &#8594;</Link>
       </article>
     </li>
   );
