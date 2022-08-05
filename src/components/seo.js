d604efe84ee57a4aff8dbaa763bd5ca6723eab4b
aishat-liasu <aishatliasu17965@gmail.com>
2022-08-05 23:17:01 +0100

    refactor: comment out image tags

--- src/components/seo.js
+++ src/components/seo.js
@@ -19,7 +19,7 @@ const SeoComponent = (props) => {
   const defaults = data?.site?.siteMetadata ?? {};
   const title = props.title || defaults.title;
   const description = props.description || defaults.description;
-  const image = new URL(props.image);
+  //const image = new URL(props.image || '');
   const url = new URL(props.path || '/', defaults.siteUrl);
 
   return (
@@ -27,18 +27,18 @@ const SeoComponent = (props) => {
       <title>{title}</title>
       <meta name="description" content={description} />
       <link rel="canonical" href={url} />
-      {image && <meta name="image" content={image} />}
+      {/* {image && <meta name="image" content={image} />} */}
 
       <meta property="og:url" content={url} />
       <meta property="og:type" content="website" />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      {image && <meta name="og:image" content={image} />}
+      {/* {image && <meta name="og:image" content={image} />} */}
 
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      {image && <meta name="twitter:image" content={image} />}
+      {/* {image && <meta name="twitter:image" content={image} />} */}
     </Helmet>
   );
 };
