d94c325974ec3609d3339f2c978d65eb6140e2cd
aishat-liasu <aishatliasu17965@gmail.com>
2022-09-30 12:21:17 +0100

    style: add whitespaces

--- src/components/project-param.js
+++ src/components/project-param.js
@@ -2,26 +2,32 @@ import * as React from 'react';
 import { projectParam } from '../styles/project-param.module.css';
 
 const ProjectParam = ({ githubRepo, liveLink, techStack }) => {
-
   return (
     <div className={projectParam}>
-        <article>
-            <h4>Tech Stack</h4>
-        {techStack.length > 0 && 
-            <ul>
-                {techStack.map((tech, i) => <li key={i}>{tech}</li> )}
-            </ul>
-        }
-        </article>
       <article>
-      <h4>Codebase</h4>
-        <a href={githubRepo} target="_blank" rel="noopener noreferrer">GitHub Repo</a>     
+        <h4>Tech Stack</h4>
+        {techStack.length > 0 && (
+          <ul>
+            {techStack.map((tech, i) => (
+              <li key={i}>{tech}</li>
+            ))}
+          </ul>
+        )}
       </article>
-      { liveLink &&
       <article>
-        <h4>Live</h4>
-        <a href={liveLink}  target="_blank" rel="noopener noreferrer">View Site</a>
-      </article>}
+        <h4>Codebase</h4>
+        <a href={githubRepo} target="_blank" rel="noopener noreferrer">
+          GitHub Repo
+        </a>
+      </article>
+      {liveLink && (
+        <article>
+          <h4>Live</h4>
+          <a href={liveLink} target="_blank" rel="noopener noreferrer">
+            View Site
+          </a>
+        </article>
+      )}
     </div>
   );
 };
