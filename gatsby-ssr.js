908fc84d487af9698abfe55f4da0ac5365826305
aishat-liasu <aishatliasu17965@gmail.com>
2023-03-14 07:41:09 +0100

    feat: wrap root element with root component

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,3 +1,10 @@
-exports.onRenderBody = ({ setHtmlAttributes }) => {
+import React from 'react';
+import RootElement from './src/components/root-element';
+
+export const wrapRootElement = ({ element }) => {
+  return <RootElement>{element}</RootElement>;
+};
+
+export const onRenderBody = ({ setHtmlAttributes }) => {
   setHtmlAttributes({ lang: 'en' });
 };
